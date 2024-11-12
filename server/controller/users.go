7cc4c39442f77ef2c5d0009d9e853af0c512699e
Robert Rizzo <robtrizzo@gmail.com>
2024-11-12 17:44:12 -0500

    fleshed out users controller. used dep inj approach to prepare for testing

--- server/controller/users.go
+++ server/controller/users.go
@@ -1,36 +1,114 @@
 package controller
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 	"os"
 	"server/model"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
 )
 
-func GetAllUsers(c echo.Context) error {
-	fmt.Printf("GetAllUsers\n")
-	users, err := model.GetAllUsers()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "error in GetAllUsers controller: %v\n", err)
-		return c.String(http.StatusInternalServerError, "Internal Server Error")
+type GetAllUsersFunc func() ([]model.User, error)
+
+func GetAllUsers(getAllUsers GetAllUsersFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		fmt.Printf("GetAllUsers\n")
+		users, err := getAllUsers()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error in GetAllUsers controller: %v\n", err)
+			return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Internal server error"})
+		}
+		return c.JSON(http.StatusOK, users)
 	}
-	return c.JSON(http.StatusOK, users)
 }
 
-func InsertUser(c echo.Context) error {
-	return c.String(http.StatusOK, "Inserted User")
+type InsertUsersFunc func(model.User) error
+
+func InsertUser(insertUser InsertUsersFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		fmt.Printf("insert user controller")
+		userToInsert := new(model.User)
+		if err := c.Bind(userToInsert); err != nil {
+			return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid request payload"})
+		}
+		if err := c.Validate(userToInsert); err != nil {
+			fmt.Printf("error validating user: %v\n", err)
+			return c.JSON(http.StatusBadRequest, err.Error())
+		}
+		if err := insertUser(*userToInsert); err != nil {
+			return c.JSON(http.StatusBadRequest, err.Error())
+		}
+		return c.JSON(http.StatusOK, map[string]string{"message": "User inserted"})
+	}
 }
 
-func GetUser(c echo.Context) error {
-	return c.String(http.StatusOK, "Get User")
+type GetUserFunc func(int64) (model.User, error)
+
+func GetUser(getUser GetUserFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		userIdStr := c.Param("userId")
+		userId, err := strconv.ParseInt(userIdStr, 10, 64)
+		if err != nil {
+			return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid user ID"})
+		}
+		user, err := getUser(userId)
+		if err != nil {
+			if errors.Is(err, model.ErrUserNotFound) {
+				return c.JSON(http.StatusNotFound, map[string]string{"error": "User not found"})
+			}
+			return c.JSON(http.StatusInternalServerError, map[string]string{"error": err.Error()})
+		}
+		return c.JSON(http.StatusOK, user)
+	}
 }
 
-func UpdateUser(c echo.Context) error {
-	return c.String(http.StatusOK, "Update User")
+type UpdateUserFunc func(int64, model.User) error
+
+func UpdateUser(updateUser UpdateUserFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		userIdStr := c.Param("userId")
+		userId, err := strconv.ParseInt(userIdStr, 10, 64)
+		if err != nil {
+			return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid user ID"})
+		}
+
+		updateData := new(model.User)
+		if err := c.Bind(updateData); err != nil {
+			return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid request payload"})
+		}
+		if err := c.Validate(updateData); err != nil {
+			fmt.Printf("error validating user: %v\n", err)
+			return c.JSON(http.StatusBadRequest, err.Error())
+		}
+		if err := updateUser(userId, *updateData); err != nil {
+			if errors.Is(err, model.ErrUserNotFound) {
+				return c.JSON(http.StatusNotFound, map[string]string{"error": "User not found"})
+			}
+			return c.JSON(http.StatusBadRequest, err.Error())
+		}
+		return c.JSON(http.StatusOK, map[string]string{"message": "User updated"})
+	}
 }
 
-func DeleteUser(c echo.Context) error {
-	return c.String(http.StatusOK, "Delete User")
+type DeleteUserFunc func(int64) error
+
+func DeleteUser(delteUser DeleteUserFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		userIdStr := c.Param("userId")
+		userId, err := strconv.ParseInt(userIdStr, 10, 64)
+		if err != nil {
+			return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid user ID"})
+		}
+		err = delteUser(userId)
+		if err != nil {
+			if errors.Is(err, model.ErrUserNotFound) {
+				return c.JSON(http.StatusNotFound, map[string]string{"error": "User not found"})
+			}
+			return c.JSON(http.StatusInternalServerError, map[string]string{"error": err.Error()})
+		}
+		return c.JSON(http.StatusOK, map[string]string{"message": "User deleted"})
+	}
 }
--- server/router/users.go
+++ server/router/users.go
@@ -2,18 +2,33 @@ package router
 
 import (
 	"server/controller"
+	"server/model"
 
+	"github.com/go-playground/validator"
 	"github.com/labstack/echo/v4"
 )
 
+type CustomValidator struct {
+	validator *validator.Validate
+}
+
+func (cv *CustomValidator) Validate(i interface{}) error {
+	if err := cv.validator.Struct(i); err != nil {
+		return err
+	}
+	return nil
+}
+
 func userRoutes(e *echo.Echo) {
 
-	usersGroup := e.Group("/users")
-	usersGroup.GET("", controller.GetAllUsers)
-	usersGroup.POST("", controller.InsertUser)
+	e.Validator = &CustomValidator{validator: validator.New()}
 
-	usersIdGroup := usersGroup.Group("/:id")
-	usersIdGroup.GET("", controller.GetUser)
-	usersIdGroup.PUT("", controller.UpdateUser)
-	usersIdGroup.DELETE("", controller.DeleteUser)
+	usersGroup := e.Group("/users")
+	usersGroup.GET("", controller.GetAllUsers(model.GetAllUsers))
+	usersGroup.POST("", controller.InsertUser(model.InsertUser))
+
+	usersIdGroup := usersGroup.Group("/:userId")
+	usersIdGroup.GET("", controller.GetUser(model.GetUser))
+	usersIdGroup.PUT("", controller.UpdateUser(model.UpdateUser))
+	usersIdGroup.DELETE("", controller.DeleteUser(model.DeleteUser))
 }
